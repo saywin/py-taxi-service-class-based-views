72d0476599c05a8674347c8c9dc6014329e10e36
Artem <stilon670@gmail.com>
2025-10-03 11:51:57 +0300

    Add views and paths to taxi

--- taxi/urls.py
+++ taxi/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import index
+from .views import index, CarListView, CarDetailView, DriverListView, DriverDetailView
 
 urlpatterns = [
     path("", index, name="index"),
+    path("cars/", CarListView.as_view(), name="car_list"),
+    path("cars/{int:pk}/", CarDetailView.as_view(), name="car_detail"),
+    path("drivers/", DriverListView.as_view(), name="driver_list"),
+    path("drivers/{int: pk}/", DriverDetailView.as_view(), name="driver_detail"),
 ]
 
 app_name = "taxi"
--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from taxi.models import Driver, Car, Manufacturer
 
@@ -13,3 +15,28 @@ def index(request):
     }
 
     return render(request, "taxi/index.html", context=context)
+
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    paginate_by = 5
+    queryset = Manufacturer.objects.order_by("name")
+
+
+class CarListView(ListView):
+    model = Car
+    queryset = Car.objects.select_related("manufacturer")
+
+
+class CarDetailView(DetailView):
+    model = Car
+
+
+class DriverListView(ListView):
+    model = get_user_model()
+    paginate_by = 5
+
+
+class DriverDetailView(DetailView):
+    model = get_user_model()
+    queryset = Driver.objects.prefetch_related("cars")
